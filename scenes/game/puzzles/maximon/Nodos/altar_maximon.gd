3874cf2988c5270651037f728d0cd29a5a26a4d2
Carlos <cammend.net@gmail.com>
2023-08-25 15:39:20 -0600

    Cambios de código

--- scenes/game/puzzles/maximon/Nodos/altar_maximon.gd
+++ scenes/game/puzzles/maximon/Nodos/altar_maximon.gd
@@ -4,18 +4,22 @@ extends Node2D
 
 @export var puzzle: Node2D
 @export var npc: CharacterBody2D
+
 @onready var player = $AnimationPlayer
 #Puedes leer más sobre nodos en éste documento: https://docs.google.com/document/d/1AiO1cmB31FSQ28me-Rb15EQni8Pyomc1Vgdm1ljL3hc
+
 
 # Función de inicialización
 func _ready():
 	visible = false
 	npc.add_dialogue_ended(_on_dialog_ended)
 
+
 # Cuando se termine de dialogar, mostrar un "altar" para acceder al puzzle
 func _on_dialog_ended():
 	visible = true
 	player.play("show")
+
 
 # Para mostrar el puzzle
 func _on_area_2d_input_event(_viewport, event: InputEvent, _shape_idx):
