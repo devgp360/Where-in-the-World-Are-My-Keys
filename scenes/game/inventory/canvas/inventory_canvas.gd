ce5edaccd4d5d221c16adef94d89152caa7086f1
Maxim Feshchuk <masimka@users.noreply.github.com>
2023-08-28 16:05:15 -0600

    revisión de script

--- scenes/game/inventory/canvas/inventory_canvas.gd
+++ scenes/game/inventory/canvas/inventory_canvas.gd
@@ -7,17 +7,17 @@ extends Node2D
 # DOCUMENTACIÓN (catálogo de objetos): https://docs.google.com/document/d/1aFTTLLd4Yb8T_ntjjGlv4LHEGgnz8exqdcbFO9XK3MA/edit?usp=drive_link
 
 # Referencia a todas las "cajas", que contienen objetos
-var item_contents = []
+var _item_contents = []
 # Referencia de todos los nombres de objetos que hay en el inventario
-var item_object_names = []
+var _item_object_names = []
 # Referencia de los items
-var item_objects = []
+var _item_objects = []
 # Referencia de items que está usando (vistiendo) el personaje
-var dressed_item_list = []
+var _dressed_item_list = []
 # "Objeto" seleccionado
-var current_item_selected = null
+var _current_item_selected = null
 # Nombre del objeto seleccionado
-var current_item_name_selected = ""
+var _current_item_name_selected = ""
 
 @onready var canvas = $CanvasLayer # Canvas principal
 @onready var animation_player = $CanvasLayer/AnimationPlayer # Player
@@ -35,7 +35,7 @@ func _ready():
 	for n in 24:
 		var item = load("res://scenes/game/inventory/item_content/item_content.tscn").instantiate()
 		grid.add_child(item) # Agregamos el nodo "marco" a un grid
-		item_contents.append(item) # Guardamos la referencia del nodo "marco" en un array
+		_item_contents.append(item) # Guardamos la referencia del nodo "marco" en un array
 
 
 # Función que se ejecuta en cada frame
@@ -77,13 +77,13 @@ func _pressed(_name: String):
 	# Funcionalidad para "quitarse/ponerse" los lentes al dar clic en el item desde el inventario
 	if _name == 'puzzle_vidriera/item_lentes':
 		var character = escena1.find_child("MainCharacter")
-		var index = dressed_item_list.find("glasses")
+		var index = _dressed_item_list.find("glasses")
 		if index >= 0:
 			character.dress_item("glasses", false)
-			dressed_item_list.remove_at(index)
+			_dressed_item_list.remove_at(index)
 		else:
 			character.dress_item("glasses", true)
-			dressed_item_list.append("glasses")
+			_dressed_item_list.append("glasses")
 
 
 # DOCUMENTACIÓN (gestión de memoria): https://docs.google.com/document/d/1diS6YOpBhLUTI9tk7YTZcH5Ha64bXg9u-PVhBXfOEz4/edit#heading=h.e2j6ax5ma83s
@@ -92,7 +92,7 @@ func _pressed(_name: String):
 # El nombre del item, tiene que existir como una escena
 func add_item_by_name(_name: String, params = null):
 	# Si el item ya existe (ya está agregado), se termina la función
-	var index = item_object_names.find(_name)
+	var index = _item_object_names.find(_name)
 	if index >= 0:
 		return
 	
@@ -104,12 +104,12 @@ func add_item_by_name(_name: String, params = null):
 		return
 	
 	# Agregamos el item al grid, y guardamos las referencias (para poder eliminarlo si es requerido)
-	index = item_object_names.size()
+	index = _item_object_names.size()
 	var item = item_to_load.instantiate()
-	var item_content = item_contents[index]
+	var item_content = _item_contents[index]
 	item_content.add_child(item)
-	item_object_names.append(_name);
-	item_objects.append(item)
+	_item_object_names.append(_name);
+	_item_objects.append(item)
 	item.pressed.connect(_pressed.bind(_name))
 	# Algunos objetos, pueden tener un método para agregarle parámetros
 	if item.has_method("add_params") and params:
@@ -122,53 +122,53 @@ func add_item_by_name(_name: String, params = null):
 # Al eliminar el nodo, todos los demás nodos posteriores, se moverán "hacia atrás"
 # para evitar dejar "espacios vacíos"
 func _remove_item_by_name(_name: String):
-	var index = item_object_names.find(_name)
+	var index = _item_object_names.find(_name)
 	if index >= 0:
-		var item_content = item_contents[index] # Nodo que es un "cuadro" contenedor del item recolectado
-		var item = item_objects[index] # Nodo que tiene el item recolectado
+		var item_content = _item_contents[index] # Nodo que es un "cuadro" contenedor del item recolectado
+		var item = _item_objects[index] # Nodo que tiene el item recolectado
 		
 		# Removemos el nodo del item
 		item_content.remove_child(item)
 		item.queue_free(); # Liberamos memoria (porque no lo vamos a volver a usar)
 		
 		# Movemos todos los items "hacia atrás" para que ocupen el espacio vacío
-		var size = item_objects.size()
+		var size = _item_objects.size()
 		for n in range(index, size - 1):
-			var current_content = item_contents[n]
-			var next_content = item_contents[n + 1]
-			var next_item = item_objects[n + 1];
+			var current_content = _item_contents[n]
+			var next_content = _item_contents[n + 1]
+			var next_item = _item_objects[n + 1];
 			# Removemos el item "next_item" (pero no liberamos memoria)
 			next_content.remove_child(next_item);
 			# El item removido anteriormente, se reutiliza (agrega) en otro nodo
 			current_content.add_child(next_item);
 		
 		# Quitamos el nombre del listado de "nombres de items"
-		item_object_names.remove_at(index)
+		_item_object_names.remove_at(index)
 		
 		# Quitamos el nodo, del listado de nodos tipo item
-		item_objects.remove_at(index)
+		_item_objects.remove_at(index)
 
 
 # Elimina todos los elementos del inventario
 func remove_all_items():
-	var size = item_object_names.size()
+	var size = _item_object_names.size()
 	for i in size:
-		_remove_item_by_name(item_object_names[size - i - 1])
+		_remove_item_by_name(_item_object_names[size - i - 1])
 
 
 # Retorna un listado de "nombres" de items que están en inventario
 func get_item_list_names():
-	return item_object_names
+	return _item_object_names
 
 
 # Retorna un listado de "nombres" de items que el personaje debe "vestir"
 func get_dressed_item_list():
-	return dressed_item_list
+	return _dressed_item_list
 
 
 # Devuelve "true", si un item está siendo "vestido" por el personaje principal
 func is_wearing(_name: String):
-	return dressed_item_list.find(_name) >= 0
+	return _dressed_item_list.find(_name) >= 0
 
 
 # Función especial para validar si se da clic en un item del puzzle de "Jardín"
@@ -186,8 +186,8 @@ func _check_press_item_puzzle_jardin(_name: String):
 		if not character:
 			return
 		var params = null
-		var index = item_object_names.find(_name)
-		var item = item_objects[index]
+		var index = _item_object_names.find(_name)
+		var item = _item_objects[index]
 		# Buscamos parámetros del item (si existen)
 		if item.has_method("get_params"):
 			params = item.get_params()
@@ -204,10 +204,10 @@ func _check_press_item_puzzle_jardin(_name: String):
 # Función que servirá para seleccionar un item a usar (sobre otro item)
 func select_item_to_use(_name: String, select: bool):
 	# Quitar el item seleccionado
-	if not select and current_item_selected:
+	if not select and _current_item_selected:
 		_remove_selected_item()
 	# Si ya está seleccionado el item, solo terminamos la función
-	if current_item_name_selected == _name:
+	if _current_item_name_selected == _name:
 		return
 	# Cargamos el nuevo item
 	var item_to_load = load("res://scenes/game/inventory/items/" + _name + ".tscn")
@@ -217,15 +217,15 @@ func select_item_to_use(_name: String, select: bool):
 	if escena:
 		_remove_selected_item() # Removemos cualquier item que pueda existir previamente
 		var item = item_to_load.instantiate()
-		var index = item_object_names.find(_name)
+		var index = _item_object_names.find(_name)
 		var params = null
-		if item_objects[index].has_method("get_params"):
-			params = item_objects[index].get_params()
+		if _item_objects[index].has_method("get_params"):
+			params = _item_objects[index].get_params()
 		# Se deberá agregar el item a la escena (luego moverlo junto al mouse)
 		# - Falta implementación
 		escena.add_child(item)
-		current_item_selected = item
-		current_item_name_selected = _name
+		_current_item_selected = item
+		_current_item_name_selected = _name
 		# Agregamos los parámetros del item (si tiene)
 		if item.has_method("add_params") and params:
 			item.add_params(_name, params)
@@ -235,24 +235,24 @@ func select_item_to_use(_name: String, select: bool):
 
 # Se remueve "la selección" de un item de inventario
 func _remove_selected_item():
-	if current_item_selected:
+	if _current_item_selected:
 		var escena = get_tree().get_root().get_node("Main")
 		if escena:
-			escena.remove_child(current_item_selected)
-		current_item_selected.queue_free() # Liberamos la memoria
-		current_item_selected = null
-		current_item_name_selected = ""
+			escena.remove_child(_current_item_selected)
+		_current_item_selected.queue_free() # Liberamos la memoria
+		_current_item_selected = null
+		_current_item_name_selected = ""
 
 
 # Procesamos un item seleccionado
 # Consiste en mostrar el objeto (junto al puntero del ratón) y moverlo en las mismas coordenadas
 func _process_item_selected():
-	if not current_item_selected:
+	if not _current_item_selected:
 		return
 	# Falta implementar la funcionalidad de mover objeto junto al puntero
-	current_item_selected.position = get_global_mouse_position()
+	_current_item_selected.position = get_global_mouse_position()
 
 
 # Retorna el nombre del objeto actualmente seleccionado
 func get_current_item_name_selected():
-	return current_item_name_selected
+	return _current_item_name_selected
