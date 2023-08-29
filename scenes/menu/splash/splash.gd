6bc238b84a912f3fe160042e393147c259f90dca
Carlos <cammend.net@gmail.com>
2023-08-29 08:22:03 -0600

    Ajustes de código

--- scenes/menu/splash/splash.gd
+++ scenes/menu/splash/splash.gd
@@ -6,49 +6,50 @@ extends Node2D
 # DOCUMENTACIÓN (¿Como hacer Splash?): https://docs.google.com/document/d/11u22RhuGheb0z2ZpwwRgEygSr7kwTPtBme_17SZQwtw
 # DOCUMENTACIÓN MANEJO DE AUDIOS: https://docs.google.com/document/d/1-RtHioFa9rFuJvsTv92m3UQGEuosRqYBV5CTjWOPg_E
 
+# Ruta a la escena a cargar cuando finalice el "splash"
+@export var path_map_scene = "res://scenes/game/levels/rooms/map/map.tscn"
 
 # Función que se llama cuando la escena esta cargada
 func _ready():
-	#Setamos los niveles de sonidos
-	setSoundsVolume()
+	# Setamos los niveles de sonidos
+	_set_sounds_volume()
 
 
-#Escuchamos el teclado
+# Cuando termina la animación
+func _on_animation_player_animation_finished(_anim_name):
+	# Llamamos el la funcion de cambio de escena
+	_go_title_screen()
+
+
+# Escuchamos el teclado
 func _input(event):
 	#Escuchamos si se preciona algun boton
-	if(event is InputEventKey):
-		#Llamamos el la funcion de cambio de escena
-		go_title_screen()
+	if event is InputEventKey:
+		# Llamamos el la funcion de cambio de escena
+		_go_title_screen()
 
 
-func setSoundsVolume():
-	#Cargamos los datos guardados
+# Función que inicializa el volumen de los sonidos (según datos guardados)
+func _set_sounds_volume():
+	# Cargamos los datos guardados
 	var data = SaveProgress.load_game()
-	#Validamos si existen los datos
+	# Validamos si existen los datos
 	if (data):
-		#Seteamos el nivel de sonido de la música
+		# Seteamos el nivel de sonido de la música
 		Global.music_vol = data.sound.music
-		#Seteamos el nivel de sonido de los sonidos de personaje principal
+		# Seteamos el nivel de sonido de los sonidos de personaje principal
 		Global.main_character_vol = data.sound.main_character
-		#Seteamos el nivel de sonido de los sonidos de NPC
+		# Seteamos el nivel de sonido de los sonidos de NPC
 		Global.npc_vol = data.sound.npc
-		#Creamos el arreglo de tipos de sonidos
-		var bus = ["music","main_character","npc"]
-		#Recorremos cada tipo de sonido
-		for i in bus.size():
-			#Obtenemos el canal de sonido
-			var music = AudioServer.get_bus_index(bus[i])
-			#Seteamos el volumen del sonido
-			AudioServer.set_bus_volume_db(music,linear_to_db(data.sound[bus[i]]))
+		# Recorremos todos los tipos de sonidos disponibles
+		for sound_type in data.sound:
+			var volume = data.sound[sound_type] # Volumen del sonido
+			var chanel = AudioServer.get_bus_index(sound_type) # Canal de sonido
+			# Seteamos el volumen del sonido
+			AudioServer.set_bus_volume_db(chanel, linear_to_db(volume))
 
 
-#Redirect a la escena de Mapa
-func go_title_screen():
-	#Pasamos a la escena de Menú principal
-	get_tree().change_scene_to_file("res://scenes/game/levels/rooms/map/map.tscn")
-
-
-#Cuando termina la animación
-func _on_animation_player_animation_finished(_anim_name):
-	#Llamamos el la funcion de cambio de escena
-	go_title_screen()
+# Redirect a la escena de Mapa
+func _go_title_screen():
+	# Pasamos a la escena de Menú principal
+	get_tree().change_scene_to_file(path_map_scene)
