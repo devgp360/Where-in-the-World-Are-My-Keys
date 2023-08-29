ab15e487826f2e3ad2a7a88916be8e5b314a2682
Carlos <cammend.net@gmail.com>
2023-08-29 14:54:19 -0600

    Ajustes de código

--- scenes/menu/main_menu/h_slider.gd
+++ scenes/menu/main_menu/h_slider.gd
@@ -16,7 +16,7 @@ func _ready():
 	#Obtenemos el index del canal del sonido
 	bus_index = AudioServer.get_bus_index(bus_name)
 	#Seteamos el nivel del sonido
-	AudioServer.set_bus_volume_db(bus_index,linear_to_db(Global[bus_name + "_vol"]))
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(Global[bus_name + "_vol"]))
 	#Seteamos el value del slider del medidor de sonido
 	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
 	#Conectamos el evento change del slider
@@ -26,7 +26,7 @@ func _ready():
 # Función que se llama cuando se hace el cambio del valor de volumen del sonido
 func _on_value_changed(_value:float) -> void:
 	#Seteamos el nivel del sonido
-	AudioServer.set_bus_volume_db(bus_index,linear_to_db(_value))
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(_value))
 	#guardamos el nivel del sonido
 	SaveProgress.save_sounds(bus_name, _value)
 	#Seteamos la variable global del sonido
